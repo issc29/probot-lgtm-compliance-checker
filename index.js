aaddd6e1abfe5acae1e8e3a35498c7d6e6acade5
Isaac Cohen <issc29@gmail.com>
2020-02-04 16:09:57 -0500

    Refactored code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const request = require('request')
+const rp = require('request-promise')
 const yaml = require('js-yaml')
 const util = require('util')
 
@@ -18,6 +18,8 @@ const languages = {
 }
 
 const LGTMToken = process.env.LGTM_TOKEN
+const LGTMURL = (process.env.LGTM_URL) ? process.env.LGTM_URL : 'https://lgtm.com/'
+const LGTMURLFormatted = (LGTMURL.charAt(LGTMURL.length - 1) === '/') ? LGTMURL : LGTMURL + '/'
 
 module.exports = app => {
   app.on(['status'], receivedStatus)
@@ -26,16 +28,54 @@ module.exports = app => {
     const startTime = new Date()
     const logger = app.log
     const params = context.repo({ path: '.github/lgtm-compliance.yml' })
-    const { state: statusState, sha: statusSha, context: statusContext, target_url: statusTargetURL } = context.payload
-    const langaugeDescription = statusContext.substring(15)
+    const { state: statusState } = context.payload
 
-    if (LGTMToken === '' || LGTMToken === undefined) {
+    if (!isValidLGTMToken) {
       return
     }
 
     console.log('Received Webhook')
     console.log('state: ' + context.payload.state + ' context: ' + context.payload.context)
     console.log(util.inspect(context.payload))
+
+    if (!isSuccessState(statusState)) {
+      createStatus(context, startTime)
+      return
+    }
+
+    context.github.repos.getContents(params).then((configContent) => {
+      const content = Buffer.from(configContent.data.content, 'base64').toString()
+      const config = yaml.safeLoad(content)
+      getLGTMCodeReview(context)
+        .then(function (body) {
+          handleLGTMRequest(body, context, config)
+        })
+        .catch(function (err) {
+          logger.error(`Error getting LGTM Code Review for for Owner: ${params.owner} Repo: ${params.repo}`)
+          logger.error(err)
+        })
+    }, (reason) => {
+      logger.info(`NOTE: config file not found for Owner: ${params.owner} Repo: ${params.repo}`)
+    })
+  }
+
+  function isValidLGTMToken () {
+    if (LGTMToken === '' || LGTMToken === undefined) {
+      return false
+    }
+    return true
+  }
+
+  function isSuccessState (statusState) {
+    if (statusState === 'pending' || statusState === 'error' || statusState === 'failure') {
+      return false
+    }
+    return true
+  }
+
+  function createStatus (context, startTime) {
+    const { state: statusState, sha: statusSha, context: statusContext, target_url: statusTargetURL } = context.payload
+    const langaugeDescription = statusContext.substring(15)
 
     if (statusState === 'pending') {
       context.github.checks.create(context.repo({
@@ -45,7 +85,6 @@ module.exports = app => {
         details_url: statusTargetURL,
         started_at: startTime
       }))
-      return
     } else if (statusState === 'error' || statusState === 'failure') {
       context.github.checks.create(context.repo({
         name: 'LGTM Compliance: ' + langaugeDescription,
@@ -60,19 +99,10 @@ module.exports = app => {
           summary: `LGTM Analysis Failure: ${context.payload.description}`
         }
       }))
-      return
     }
-
-    context.github.repos.getContents(params).then((configContent) => {
-      const content = Buffer.from(configContent.data.content, 'base64').toString()
-      const config = yaml.safeLoad(content)
-      getCodeReview(context, config)
-    }, (reason) => {
-      logger.info('NOTE: config file not found')
-    })
   }
 
-  function getCodeReview (context, config) {
+  function getLGTMCodeReview (context) {
     const { target_url: statusTargetURL } = context.payload
 
     // Probot API note: context.repo() => {username: 'hiimbex', repo: 'testing-things'}
@@ -80,9 +110,9 @@ module.exports = app => {
     var codereviewID = statusTargetURL.substring(start + 8)
 
     console.log('Success')
-
+    console.log(`LGTM_URL = ${LGTMURLFormatted} | URL = ${LGTMURLFormatted}api/v1.0/codereviews/${codereviewID}`)
     const options = {
-      url: 'https://lgtm.octodemo.com/api/v1.0/codereviews/' + codereviewID,
+      url: `${LGTMURLFormatted}api/v1.0/codereviews/${codereviewID}`,
       method: 'GET',
       headers: {
         Accept: 'application/json',
@@ -90,9 +120,7 @@ module.exports = app => {
       }
     }
 
-    request(options, function (err, res, body) {
-      handleLGTMRequest(err, body, context, config)
-    })
+    return rp(options)
   }
 
   function isSevere (minSeverity, severity) {
@@ -125,10 +153,9 @@ module.exports = app => {
     return false
   }
 
-  async function handleLGTMRequest (err, body, context, config) {
+  async function handleLGTMRequest (body, context, config) {
     const { sha: statusSha, context: statusContext, target_url: statusTargetURL } = context.payload
     console.log(body)
-    console.error('error:', err) // Print the error if one occurred
 
     var json = JSON.parse(body)
     const langaugeDescription = statusContext.substring(15)
@@ -138,49 +165,15 @@ module.exports = app => {
 
     const minSeverity = config[languageCode].severity
     const requiredTags = config[languageCode].tags
-    var severeAlerts = []
-
-    javascriptElement.alerts.forEach(function (element) {
-      if (!isNewAlert(element)) {
-        return
-      }
-
-      if (!isSevere(minSeverity, element.query.properties.severity)) {
-        return
-      }
-
-      if (!containsRequiredTags(requiredTags, element.query.properties.tags)) {
-        return
-      }
-
-      severeAlerts.push(element)
-    })
+    var severeAlerts = getAlerts(javascriptElement.alerts, minSeverity, requiredTags)
+    var conclusion = 'success'
+    var title = 'LGTM Compliance Passed'
+    var summary = 'LGTM Compliance Passed'
 
     if (severeAlerts.length > 0) {
-      var summaryText = `This pull request **introduces ${severeAlerts.length} alert ${(severeAlerts.length > 1) ? 's' : ''}** - [view on LGTM](${statusTargetURL})`
-
-      summaryText += '\n**new alerts:**'
-      severeAlerts.forEach(function (severeAlert) {
-        summaryText += `\n**Name**: ${severeAlert.query.name} **Severity**: ${severeAlert.query.properties.severity} **Tags**: ${severeAlert.query.properties.tags.toString()}`
-      })
-      const { data: pendingCheck } = await context.github.checks.listForRef(context.repo({
-        ref: statusSha,
-        check_name: `LGTM Compliance: ${langaugeDescription}`
-      }))
-
-      console.log('Pending Check')
-      console.log(pendingCheck)
-
-      return context.github.checks.update(context.repo({
-        check_run_id: pendingCheck.check_runs[0].id,
-        status: 'completed',
-        conclusion: 'failure',
-        completed_at: new Date(),
-        output: {
-          title: `LGTM Compliance Failed: ${severeAlerts.length} new alert`,
-          summary: summaryText
-        }
-      }))
+      summary = formatSummaryText(severeAlerts, statusTargetURL)
+      conclusion = 'failure'
+      title = `LGTM Compliance Failed: ${severeAlerts.length} new alert`
     }
 
     const { data: pendingCheck } = await context.github.checks.listForRef(context.repo({
@@ -194,17 +187,47 @@ module.exports = app => {
     return context.github.checks.update(context.repo({
       check_run_id: pendingCheck.check_runs[0].id,
       status: 'completed',
-      conclusion: 'success',
+      conclusion: conclusion,
       completed_at: new Date(),
       output: {
-        title: 'LGTM Compliance Passed',
-        summary: 'LGTM Compliance Passed'
+        title: title,
+        summary: summary
       }
     }))
   }
 
   function isNewAlert (alertElement) {
     return (alertElement.new > 0)
+  }
+
+  function getAlerts (alerts, minSeverity, requiredTags) {
+    var severeAlerts = []
+    alerts.forEach(function (alert) {
+      if (!isNewAlert(alert)) {
+        return
+      }
+
+      if (!isSevere(minSeverity, alert.query.properties.severity)) {
+        return
+      }
+
+      if (!containsRequiredTags(requiredTags, alert.query.properties.tags)) {
+        return
+      }
+
+      severeAlerts.push(alert)
+    })
+    return severeAlerts
+  }
+
+  function formatSummaryText (severeAlerts, codeReviewURL) {
+    var summaryText = `This pull request **introduces ${severeAlerts.length} alert ${(severeAlerts.length > 1) ? 's' : ''}** - [view on LGTM](${codeReviewURL})`
+
+    summaryText += '\n**new alerts:**'
+    severeAlerts.forEach(function (severeAlert) {
+      summaryText += `\n**Name**: ${severeAlert.query.name} **Severity**: ${severeAlert.query.properties.severity} **Tags**: ${severeAlert.query.properties.tags.toString()}`
+    })
+    return summaryText
   }
 
   // For more information on building apps:
